cc686630ba95f996438f86b2b367a57ac0a93646
NguyenNgocThuanThien8059 <143669066+NguyenNgocThuanThien8059@users.noreply.github.com>
2024-11-18 15:55:40 +0700

    Add files via upload

---
+++ VPNServer/Encryption Enhancements/VPNServer/VPNServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VPNServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VPN SERVER");
+            // Setup server encryption managers
+            EncryptionManager encryptionManager = new EncryptionManager();
+            ServerEncryptionManager secureCommServer = new ServerEncryptionManager();
+
+            // Setup TCP listener for client connections
+            TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+            listener.Start();
+            Console.WriteLine("Server started. Waiting for client...");
+
+            // Accept client connection
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+
+            // Get network stream to communicate with the client
+            NetworkStream stream = client.GetStream();
+
+            // 1. Send the server's public RSA key to the client
+            string publicKey = encryptionManager.GetPublicKey();
+            byte[] publicKeyBytes = Encoding.UTF8.GetBytes(publicKey);
+            stream.Write(publicKeyBytes, 0, publicKeyBytes.Length);
+            Console.WriteLine("Server's public key sent to client.");
+
+            // 2. Receive the encrypted AES key and IV from the client
+            byte[] buffer = new byte[256];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            byte[] encryptedAESKey = new byte[bytesRead];
+            Array.Copy(buffer, 0, encryptedAESKey, 0, bytesRead);
+
+            // Receive IV (Initialization Vector)
+            bytesRead = stream.Read(buffer, 0, buffer.Length);
+            byte[] iv = new byte[bytesRead];
+            Array.Copy(buffer, 0, iv, 0, bytesRead);
+
+            // 3. Decrypt the AES key
+            secureCommServer.SetAESKey(encryptedAESKey, iv, encryptionManager);
+            Console.WriteLine("AES key received and decrypted.");
+
+            // 4. Receive encrypted message from the client
+            bytesRead = stream.Read(buffer, 0, buffer.Length);
+            byte[] encryptedMessage = new byte[bytesRead];
+            Array.Copy(buffer, 0, encryptedMessage, 0, bytesRead);
+
+            // 5. Decrypt the received message
+            string decryptedMessage = secureCommServer.DecryptData(encryptedMessage);
+            Console.WriteLine("Decrypted message from client: " + decryptedMessage);
+
+            // Close connection
+            client.Close();
+            listener.Stop();
+            Console.WriteLine("Server closed.");
+        }
+    }
+}
---
+++ VPNServer/Encryption Enhancements/VPNServer/VPNServer/ServerEncryptionManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace VPNServer
+{
+    public class ServerEncryptionManager
+    {
+        private Aes aes;
+        public ServerEncryptionManager()
+        {
+            aes = Aes.Create();
+            aes.KeySize = 256;
+        }
+        // Decrypt the AES key received from the client
+        public void SetAESKey(byte[] encryptedKey, byte[] iv, EncryptionManager encryptionManager)
+        {
+            aes.Key = encryptionManager.DecryptData(encryptedKey);
+            aes.IV = iv;
+        }
+        // Decrypt data using AES
+        public string DecryptData(byte[] encryptedData)
+        {
+            using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+            {
+                byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+                return System.Text.Encoding.UTF8.GetString(decryptedBytes);
+            }
+        }
+    }
+}
