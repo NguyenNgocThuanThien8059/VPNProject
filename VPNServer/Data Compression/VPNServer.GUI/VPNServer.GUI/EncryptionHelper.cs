3e4dcbeaaf5cf13737a21345bba514ca5c1891df
NguyenNgocThuanThien8059 <143669066+NguyenNgocThuanThien8059@users.noreply.github.com>
2024-11-23 18:31:54 +0700

    Add files via upload

---
+++ VPNServer/Data Compression/VPNServer.GUI/VPNServer.GUI/EncryptionHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPNServer.GUI
+{
+    public static class EncryptionHelper
+    {
+        private static string key = "abcdefghijklmnop"; // 16-byte key (128-bit)
+        private static string iv = "1234567890123456"; // 16-byte IV (Initialization Vector)
+
+        // Encrypt data
+        public static string Encrypt(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    // Compress data before returning
+                    return Convert.ToBase64String(Compress(msEncrypt.ToArray()));
+                }
+            }
+        }
+
+        // Decrypt data
+        public static string Decrypt(string cipherText)
+        {
+            // Decompress data after converting from Base64
+            byte[] decompressedData = Decompress(Convert.FromBase64String(cipherText));
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(decompressedData))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        // Compress data using GZip
+        private static byte[] Compress(byte[] data)
+        {
+            using (MemoryStream output = new MemoryStream())
+            {
+                using (GZipStream gzip = new GZipStream(output, CompressionMode.Compress))
+                {
+                    gzip.Write(data, 0, data.Length);
+                }
+                return output.ToArray();
+            }
+        }
+
+        // Decompress data using GZip
+        private static byte[] Decompress(byte[] data)
+        {
+            using (MemoryStream input = new MemoryStream(data))
+            using (GZipStream gzip = new GZipStream(input, CompressionMode.Decompress))
+            using (MemoryStream output = new MemoryStream())
+            {
+                gzip.CopyTo(output);
+                return output.ToArray();
+            }
+        }
+    }
+}
